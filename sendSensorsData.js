51f0117a5de96c6196370d58e7bd17fb0a6adc68
mritunjay7497 <mkumarmani97@gmail.com>
2022-01-31 22:36:42 +0530

    First and final commit to transfer sensor data of ubuntu to google spreadsheet.

---
+++ sendSensorsData.js
@@ -0,0 +1,34 @@
+#!/usr/bin/zsh
+
+const execSync = require("child_process").execSync;
+const fs = require("fs");
+const spawn = require("child_process").spawn;
+
+let commandObject = {
+    "sensor":"sensors -Aj"
+}
+
+let commandOutput = {};
+
+commandOutput[Object.keys(commandObject)[0]] = JSON.parse(execSync(commandObject.sensor));
+
+let sensorInfo = {}
+if(commandOutput["sensor"]["iwlwifi_1-virtual-0"]){
+    sensorInfo["wifi-chip-temp"] = commandOutput["sensor"]["iwlwifi_1-virtual-0"]["temp1"]["temp1_input"];
+}
+if(commandOutput["sensor"]["nvme-pci-0600"]){
+    sensorInfo["ssd-composite-temp"] = commandOutput["sensor"]["nvme-pci-0600"]["Composite"]["temp1_input"];
+    sensorInfo["ssd-sensor1-temp"] = commandOutput["sensor"]["nvme-pci-0600"]["Sensor 1"]["temp2_input"];
+    sensorInfo["ssd-sensor2-temp"] = commandOutput["sensor"]["nvme-pci-0600"]["Sensor 2"]["temp3_input"];
+}
+if(commandOutput["sensor"]["coretemp-isa-0000"]){
+    sensorInfo["cpu-0-temp"] = commandOutput["sensor"]["coretemp-isa-0000"]["Core 0"]["temp2_input"];
+    sensorInfo["cpu-1-temp"] = commandOutput["sensor"]["coretemp-isa-0000"]["Core 1"]["temp3_input"];
+    sensorInfo["cpu-2-temp"] = commandOutput["sensor"]["coretemp-isa-0000"]["Core 2"]["temp4_input"];
+    sensorInfo["cpu-2-temp"] = commandOutput["sensor"]["coretemp-isa-0000"]["Core 3"]["temp5_input"];
+}
+
+fs.writeFile("sensors.json", JSON.stringify(sensorInfo,undefined,2),() => {
+    console.log("starting upload routine...\n")
+    spawn("python3",["/home/plusx/sendSensorData/uploadToGoogleSheets.py"]);
+});
---
+++ uploadToGoogleSheets.py
@@ -0,0 +1,78 @@
+from __future__ import print_function
+from multiprocessing.connection import Client
+
+import os.path
+from pickle import GLOBAL
+from pprint import pprint
+import json
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+
+sheetId = {}
+
+with open("sensitiveInfo.json", "r") as sensitiveInfo:
+    sheetId['spreadheetId'] = json.load(sensitiveInfo)
+
+SPREADSHEET_ID = sheetId['spreadheetId']['SPREADHSEET_ID']
+RANGE_NAME = 'Sheet1'
+
+
+def main():
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                '/home/plusx/sendSensorData/credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    try:
+        service = build('sheets', 'v4', credentials=creds)
+
+        # Call the Sheets API
+        sheet = service.spreadsheets()
+        result = sheet.values().get(spreadsheetId=SPREADSHEET_ID,
+                                    range=RANGE_NAME).execute()
+
+        value_input_option = 'USER_ENTERED'
+
+        sensorJson = {}
+        with open("sensors.json", "r") as sj:
+            sensorJson = json.load(sj)
+
+        currentReading = []
+
+        for sensor in sensorJson.keys():
+            currentReading.append([sensor, sensorJson[sensor]])
+
+        request = service.spreadsheets().values().update(spreadsheetId=SPREADSHEET_ID,
+                                                         range=RANGE_NAME, valueInputOption=value_input_option, body={"values": currentReading})
+
+        response = request.execute()
+
+        pprint(response)
+
+    except HttpError as err:
+        print(err)
+
+
+if __name__ == '__main__':
+    main()
+    os.system("rm sensors.json")
